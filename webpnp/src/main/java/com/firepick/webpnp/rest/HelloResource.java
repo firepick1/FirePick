5b7b3bbc9bd73d4c56063eeddb6c6e1370a87b6d
Karl Lew <karl@firepick.org>
2013-05-21 22:02:50 -0700

    Jersey

--- webpnp/src/main/java/com/firepick/webpnp/rest/HelloResource.java
+++ webpnp/src/main/java/com/firepick/webpnp/rest/HelloResource.java
@@ -3,6 +3,7 @@ package com.firepick.webpnp.rest;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import java.util.Date;
 
 @Path("/helloworld")
 public class HelloResource {
@@ -10,7 +11,7 @@ public class HelloResource {
     @GET
     @Produces("text/plain")
     public String getClichedMessage() {
-        // Return some cliched textual content
-        return "Hello World";
+        Date date = new Date();
+        return "Hello World" + date;
     }
 }
